92e25c529584c11947f4f36d26c60a386957c300
Fabricio Machado <30887617+fbrcmachado@users.noreply.github.com>
2020-11-12 22:15:35 -0300

    Update consultas.py
    Adição de comentário.

--- consultas.py
+++ consultas.py
@@ -1,5 +1,9 @@
 class Consultas():
     
+    ## Essa classe serve para retornar o texto de uma consulta específica. Futuramente esse arquivo pode ser separado em vários, por contexto. 
+    ## exemplo: todas as consultas de uma determinada tabela, todas as consultas de um determinado banco... com isso, caso o sistema cresça, as 
+    ## consultas estarão centralizadas em um único ponto, facilitando a manutenção. 
+    
     def GetAll():
         return "SELECT * FROM TABELA"
     
