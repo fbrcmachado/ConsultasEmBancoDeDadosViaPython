34fbf6bed5e9ece5da6507b51f45f7075b4f6603
Fabricio Machado <30887617+fbrcmachado@users.noreply.github.com>
2020-11-12 22:27:01 -0300

    Update databases.py
    Adicionando comentários.

--- databases.py
+++ databases.py
@@ -1,3 +1,4 @@
+## Esses são os contextos, representando o banco que se deseja acessar. Pode-se melhorar e adicionar essa separação por servidor. 
 class Contextos:
     def Banco1():
         return "Banco1"
